22ea4e00a5d2541639811a350694e790a9d62e79
Ansh Srivastava <anshsriv251107@gmail.com>
2026-09-17 10:54:29 +0530

    Convert modified Kaprekar number algorithm to C++

--- Algorithms/Implementation/modified-kaprekar-numbers.py
+++ Algorithms/Implementation/modified-kaprekar-numbers.py
@@ -1,30 +1,41 @@
-'''
-A modified Kaprekar number is a positive whole number n with d digits, such that when we split its square into two pieces - a right hand piece r with d digits and a left hand piece l that contains the remaining d or d−1 digits, the sum of the pieces is equal to the original number (i.e. l + r = n).
+#include <bits/stdc++.h>
+using namespace std;
 
-Note: r may have leading zeros.
+bool isKaprekar(long long n) {
+    long long square = n * n;
 
-Here's an explanation from Wikipedia about the ORIGINAL Kaprekar Number (spot the difference!): In mathematics, a Kaprekar number for a given base is a non-negative integer, the representation of whose square in that base can be split into two parts that add up to the original number again. For instance, 45 is a Kaprekar number, because 45² = 2025 and 20+25 = 45.
+    // Count digits of n
+    int d = to_string(n).length();
 
-The Task
-You are given the two positive integers p and q, where p is lower than q. Write a program to determine how many Kaprekar numbers are there in the range between p and q (both inclusive) and display them all.
-'''
+    // 10^d
+    long long power = 1;
+    for (int i = 0; i < d; i++) {
+        power *= 10;
+    }
 
-def kaprekars():
-	kaps = []
-	low = int(input())
-	high = int(input())
-	for num in range(low, high + 1):
-		if is_kap(num):
-			kaps.append(num)
-	if len(kaps) == 0:
-		print("INVALID RANGE")
-	else:
-		print(" ".join(str(x) for x in kaps))
+    // Split square into left and right parts
+    long long right = square % power;
+    long long left = square / power;
 
-def is_kap(num):
-	s = str(num**2)
-	right = s[len(s)//2:]
-	left = '0' if s[:len(s)//2] == '' else s[:len(s)//2]
-	return True if int(left) + int(right) == num else False
+    return (left + right == n);
+}
 
-kaprekars()
+int main() {
+    long long p, q;
+    cin >> p >> q;
+
+    bool found = false;
+
+    for (long long n = p; n <= q; n++) {
+        if (isKaprekar(n)) {
+            cout << n << " ";
+            found = true;
+        }
+    }
+
+    if (!found) {
+        cout << "INVALID RANGE";
+    }
+
+    return 0;
+}
